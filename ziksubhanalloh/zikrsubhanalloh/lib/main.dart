7a103ec9c86e2c66e778b662daf933c71c646aec
Maryamxon-flutter-developer <maryamxonmeliboyeva@gmail.com>
2024-08-25 21:16:46 +0500

    zikr

--- ziksubhanalloh/zikrsubhanalloh/lib/main.dart
+++ ziksubhanalloh/zikrsubhanalloh/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(MaterialApp(home: MyWidget()));
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyWidget(),
+    ));
 
 class MyWidget extends StatefulWidget {
   const MyWidget({super.key});
@@ -11,34 +15,106 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
-  final AudioPlayer _audioPlayer = AudioPlayer();
+  final player = AudioPlayer();
 
   int subhanallahCount = 0;
   int alhamdulillahCount = 0;
   int allahuAkbarCount = 0;
   int laIlahaIllallahCount = 0;
 
+  int subhanallahTotal = 0;
+  int alhamdulillahTotal = 0;
+  int allahuAkbarTotal = 0;
+  int laIlahaIllallahTotal = 0;
+
   @override
-  void dispose() {
-    _audioPlayer.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+    _loadCounts(); // Zikrlar sonini yuklash
   }
 
-  Future<void> playSound(String soundPath) async {
-    try {
-      await _audioPlayer.play(AssetSource(soundPath));
-    } catch (e) {
-      print("Error playing sound: $e");
-    }
+  Future<void> _loadCounts() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      subhanallahCount = prefs.getInt('subhanallahCount') ?? 0;
+      alhamdulillahCount = prefs.getInt('alhamdulillahCount') ?? 0;
+      allahuAkbarCount = prefs.getInt('allahuAkbarCount') ?? 0;
+      laIlahaIllallahCount = prefs.getInt('laIlahaIllallahCount') ?? 0;
+
+      subhanallahTotal = prefs.getInt('subhanallahTotal') ?? 0;
+      alhamdulillahTotal = prefs.getInt('alhamdulillahTotal') ?? 0;
+      allahuAkbarTotal = prefs.getInt('allahuAkbarTotal') ?? 0;
+      laIlahaIllallahTotal = prefs.getInt('laIlahaIllallahTotal') ?? 0;
+    });
+  }
+
+  Future<void> _saveCounts() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('subhanallahCount', subhanallahCount);
+    prefs.setInt('alhamdulillahCount', alhamdulillahCount);
+    prefs.setInt('allahuAkbarCount', allahuAkbarCount);
+    prefs.setInt('laIlahaIllallahCount', laIlahaIllallahCount);
+
+    prefs.setInt('subhanallahTotal', subhanallahTotal);
+    prefs.setInt('alhamdulillahTotal', alhamdulillahTotal);
+    prefs.setInt('allahuAkbarTotal', allahuAkbarTotal);
+    prefs.setInt('laIlahaIllallahTotal', laIlahaIllallahTotal);
+  }
+
+  void _incrementCount(String zikr) {
+    setState(() {
+      if (zikr == "Subhanallah") {
+        subhanallahCount++;
+        if (subhanallahCount == 33) {
+          subhanallahTotal += 33;
+          subhanallahCount = 0;
+        }
+      } else if (zikr == "Alhamdulillah") {
+        alhamdulillahCount++;
+        if (alhamdulillahCount == 33) {
+          alhamdulillahTotal += 33;
+          alhamdulillahCount = 0;
+        }
+      } else if (zikr == "Allahu Akbar") {
+        allahuAkbarCount++;
+        if (allahuAkbarCount == 33) {
+          allahuAkbarTotal += 33;
+          allahuAkbarCount = 0;
+        }
+      } else if (zikr == "La ilaha illalloh") {
+        laIlahaIllallahCount++;
+        if (laIlahaIllallahCount == 33) {
+          laIlahaIllallahTotal += 33;
+          laIlahaIllallahCount = 0;
+        }
+      }
+      _saveCounts(); // Ma'lumotni saqlash
+      player.play(AssetSource('botton.mp3'));
+    });
+  }
+
+  void _resetCount(String zikr) {
+    setState(() {
+      if (zikr == "Subhanallah") {
+        subhanallahCount = 0;
+      } else if (zikr == "Alhamdulillah") {
+        alhamdulillahCount = 0;
+      } else if (zikr == "Allahu Akbar") {
+        allahuAkbarCount = 0;
+      } else if (zikr == "La ilaha illalloh") {
+        laIlahaIllallahCount = 0;
+      }
+      _saveCounts(); // Ma'lumotni saqlash
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 1, 13, 44),
+        backgroundColor: Color.fromARGB(255, 44, 38, 1),
         title: const Text(
-          "Saved Dhikr",
+          "Saved Zikr",
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
@@ -52,43 +128,23 @@ class _MyWidgetState extends State<MyWidget> {
               height: 700,
               decoration: const BoxDecoration(
                 image: DecorationImage(
-                  image: NetworkImage(
-                    "https://png.pngtree.com/background/20210712/original/pngtree-blue-textured-muslim-background-picture-image_1174819.jpg",
-                  ),
-                  fit: BoxFit.cover,
+                  image: AssetImage("assets/art.jpg"),
+                  fit: BoxFit.fill,
                 ),
               ),
               child: Padding(
-                padding: const EdgeInsets.only(top: 200),
+                padding: const EdgeInsets.only(top: 270),
                 child: Column(
                   children: [
-                    buildDhikrContainer("Subhanallah", subhanallahCount, () {
-                      setState(() {
-                        subhanallahCount++;
-                      });
-                      playSound("assets/botton.mp3");
-                    }),
+                    buildDhikrContainer("Subhanallah", subhanallahCount, subhanallahTotal),
                     const SizedBox(height: 20),
-                    buildDhikrContainer("Alhamdulillah", alhamdulillahCount, () {
-                      setState(() {
-                        alhamdulillahCount++;
-                      });
-                      playSound("assets/botton.mp3");
-                    }),
+                    buildDhikrContainer("Alhamdulillah", alhamdulillahCount, alhamdulillahTotal),
                     const SizedBox(height: 20),
-                    buildDhikrContainer("Allahu Akbar", allahuAkbarCount, () {
-                      setState(() {
-                        allahuAkbarCount++;
-                      });
-                      playSound("assets/botton.mp3");
-                    }),
+                    buildDhikrContainer("Allahu Akbar", allahuAkbarCount, allahuAkbarTotal),
                     const SizedBox(height: 20),
-                    buildDhikrContainer("La ilaha illalloh", laIlahaIllallahCount, () {
-                      setState(() {
-                        laIlahaIllallahCount++;
-                      });
-                      playSound("assets/botton.mp3");
-                    }),
+                    buildDhikrContainer("La ilaha illalloh", laIlahaIllallahCount, laIlahaIllallahTotal),
+                    SizedBox(height: 10,),
+                  
                   ],
                 ),
               ),
@@ -99,15 +155,15 @@ class _MyWidgetState extends State<MyWidget> {
     );
   }
 
-  Widget buildDhikrContainer(String dhikr, int count, VoidCallback onPressed) {
+  Widget buildDhikrContainer(String dhikr, int count, int total) {
     return Container(
       width: 300,
       height: 70,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
-        color: Colors.black,
+        color: Color.fromARGB(255, 121, 115, 106),
         border: Border.all(
-          color: const Color.fromARGB(255, 125, 117, 46),
+          color: Color.fromARGB(255, 168, 157, 59),
           width: 5,
         ),
       ),
@@ -119,25 +175,34 @@ class _MyWidgetState extends State<MyWidget> {
             Text(
               dhikr,
               style: const TextStyle(
-                color: Color.fromARGB(255, 138, 129, 47),
+                color: Color.fromARGB(255, 250, 250, 247),
                 fontSize: 20,
               ),
             ),
             Row(
               children: [
                 Text(
-                  '$count',
+                  '$count / $total',
                   style: const TextStyle(
                     color: Colors.white,
                     fontSize: 18,
                   ),
                 ),
                 IconButton(
-                  onPressed: onPressed,
-                  icon: const Icon(Icons.add, color: Colors.white),
+                  onPressed: () {
+                    _incrementCount(dhikr);
+                  },
+                  icon: const Icon(Icons.add, color: Color.fromARGB(255, 255, 253, 253)),
+                ),
+                IconButton(
+                  onPressed: () {
+                    _resetCount(dhikr);
+                  },
+                  icon: const Icon(Icons.cancel, color: Color.fromARGB(255, 227, 223, 223)),
                 ),
               ],
             ),
+            
           ],
         ),
       ),
